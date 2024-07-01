c4506e8507b9881910ac2f09b1d107df08921d53
Ahmad <ahmadabbas434@gmail.com>
2024-07-01 10:58:00 +0500

    Buy Item

--- mini project.py
+++ mini project.py
@@ -10,4 +10,13 @@ def add_item():
    count=int(input("Enter the count of the item: "))
    inventory[name]={"price":price, "count":count}
    return print(inventory)
+def buy_item():
+    name=input("Enter the name of the item: ")
+    count=int(input("Enter the count of the item: "))
+    if name in inventory:
+        if inventory[name]["count"]>=count:
+            inventory[name]["count"]-=count
+            print("Yahooo! You have purchased this item successfully")
+        else:
+            print("item not available")
     
